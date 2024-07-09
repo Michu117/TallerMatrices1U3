ba6c9dd7a285e029e1f2075e7ed55adb610cdf93
Michu117 <fabricio.ruiz@unl.edu.ec>
2024-07-09 14:34:17 -0500

    Update 15_cartas.c

--- ejerciciosM1/15_cartas.c
+++ ejerciciosM1/15_cartas.c
@@ -2,128 +2,123 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Función para generar números aleatorios únicos del 1 al 13
-void generar_numeros_aleatorios(int arreglo[], int tam) {
-    int numeros_disponibles[tam];
-    int indice, num_generado;
-
-    // Inicializar los números disponibles del 1 al tam
-    for (indice = 0; indice < tam; ++indice) {
-        numeros_disponibles[indice] = indice + 1;
-    }
-
-    // Generar números aleatorios únicos
-    for (indice = 0; indice < tam; ++indice) {
-        int posicion = rand() % (tam - indice);
-        num_generado = numeros_disponibles[posicion];
-        arreglo[indice] = num_generado;
-
-        // Eliminar el número generado de los disponibles
-        numeros_disponibles[posicion] = numeros_disponibles[tam - indice - 1];
-    }
-}
-
-// Función para ordenar un arreglo de menor a mayor
-void ordenar_arreglo(int arreglo[], int tam) {
-    int i, j, temp;
-    for (i = 0; i < tam - 1; ++i) {
-        for (j = i + 1; j < tam; ++j) {
-            if (arreglo[i] > arreglo[j]) {
-                temp = arreglo[i];
-                arreglo[i] = arreglo[j];
-                arreglo[j] = temp;
+// Función para ordenar un arreglo usando el método de burbuja (bubblesort)
+void ordenar(int tamaño, int matriz[]) {
+    int temp;
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (matriz[j] > matriz[j + 1]) {
+                temp = matriz[j];
+                matriz[j] = matriz[j + 1];
+                matriz[j + 1] = temp;
             }
         }
     }
 }
 
-// Función para imprimir un arreglo de cartas con su etiqueta de palo
-void imprimir_arreglo(int arreglo[], int tam, char palo) {
-    for (int i = 0; i < tam; ++i) {
-        printf("%d%c ", arreglo[i], palo);
+// Función para verificar que los números no se repiten
+int buscar_num(int matriz[], int numero, int tamaño) {
+    for (int i = 0; i < tamaño; i++) {
+        if (matriz[i] == numero) {
+            return 1;
+        }
     }
-    printf("\n");
+    return 0;
 }
 
-// Función para combinar todos los arreglos de cartas en uno solo
-void combinar_todos_los_arreglos(int picas[], int corazones[], int treboles[], int diamantes[], int arreglo_combinado[], char palos_combinados[], int tam) {
-    int i, j = 0;
-
-    for (i = 0; i < tam; ++i) {
-        arreglo_combinado[j] = picas[i];
-        palos_combinados[j++] = 'P';
-    }
-    for (i = 0; i < tam; ++i) {
-        arreglo_combinado[j] = corazones[i];
-        palos_combinados[j++] = 'C';
-    }
-    for (i = 0; i < tam; ++i) {
-        arreglo_combinado[j] = treboles[i];
-        palos_combinados[j++] = 'T';
-    }
-    for (i = 0; i < tam; ++i) {
-        arreglo_combinado[j] = diamantes[i];
-        palos_combinados[j++] = 'D';
+// Función para generar números aleatorios únicos
+void generar_numeros(int tamaño, int matriz[]) {
+    int numero;
+    for (int i = 0; i < tamaño; i++) {
+        do {
+            numero = (rand() % 13) + 1; // Genera un número aleatorio entre 1 y 13
+        } while (buscar_num(matriz, numero, i) == 1); // Verifica si el número ya está en la matriz
+        matriz[i] = numero; // Asigna el número a la matriz
     }
 }
 
-// Función para imprimir el arreglo combinado de cartas ordenadas agrupadas por palo
-void imprimir_arreglo_agrupado(int picas[], int corazones[], int treboles[], int diamantes[], int tam) {
-    for (int i = 0; i < tam; ++i) {
-        printf("%dP ", picas[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < tam; ++i) {
-        printf("%dC ", corazones[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < tam; ++i) {
-        printf("%dT ", treboles[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < tam; ++i) {
-        printf("%dD ", diamantes[i]);
-    }
-    printf("\n");
-}
-
+// Función principal
 int main() {
     const int NUM_PALOS = 4;
     const int NUM_CARTAS = 13;
+    srand(time(0));
 
-    int picas[NUM_CARTAS];
-    int corazones[NUM_CARTAS];
-    int treboles[NUM_CARTAS];
-    int diamantes[NUM_CARTAS];
+    int matriz_madre[NUM_PALOS][NUM_CARTAS];
+    int matriz1[NUM_CARTAS];
+    int matriz2[NUM_CARTAS];
+    int matriz3[NUM_CARTAS];
+    int matriz4[NUM_CARTAS];
 
-    srand(time(NULL)); // Inicializar la semilla para números aleatorios
+    generar_numeros(NUM_CARTAS, matriz1);
+    generar_numeros(NUM_CARTAS, matriz2);
+    generar_numeros(NUM_CARTAS, matriz3);
+    generar_numeros(NUM_CARTAS, matriz4);
 
-    // Generar y mostrar los arreglos de cartas para cada palo (sin ordenar)
-    generar_numeros_aleatorios(picas, NUM_CARTAS);
-    generar_numeros_aleatorios(corazones, NUM_CARTAS);
-    generar_numeros_aleatorios(treboles, NUM_CARTAS);
-    generar_numeros_aleatorios(diamantes, NUM_CARTAS);
-    printf("\nEjercicio 15 (Ordenar Cartas) \n");
-    printf("Arreglos generados sin ordenar:\n");
-    printf("Picas: ");
-    imprimir_arreglo(picas, NUM_CARTAS, 'P');
-    printf("Corazones: ");
-    imprimir_arreglo(corazones, NUM_CARTAS, 'C');
-    printf("Treboles: ");
-    imprimir_arreglo(treboles, NUM_CARTAS, 'T');
-    printf("Diamantes: ");
-    imprimir_arreglo(diamantes, NUM_CARTAS, 'D');
-    printf("\n");
+    // Rellena la matriz madre con números aleatorios
+    for (int i = 0; i < NUM_CARTAS; i++) {
+        matriz_madre[0][i] = matriz1[i];
+        matriz_madre[1][i] = matriz2[i];
+        matriz_madre[2][i] = matriz3[i];
+        matriz_madre[3][i] = matriz4[i];
+    }
 
-    // Ordenar los arreglos de cada palo
-    ordenar_arreglo(picas, NUM_CARTAS);
-    ordenar_arreglo(corazones, NUM_CARTAS);
-    ordenar_arreglo(treboles, NUM_CARTAS);
-    ordenar_arreglo(diamantes, NUM_CARTAS);
-    
-    // Imprimir el arreglo combinado de cartas ordenadas agrupadas por palo
-    printf("Arreglo combinado de cartas ordenadas:\n");
-    imprimir_arreglo_agrupado(picas, corazones, treboles, diamantes, NUM_CARTAS);
+    // Imprime la matriz con números aleatorios
+    printf("Las cartas en desorden se ubican asi:\n");
+    for (int i = 0; i < NUM_PALOS; i++) {
+        for (int j = 0; j < NUM_CARTAS; j++) {
+            if (matriz_madre[i][j] == 1 || matriz_madre[i][j] == 11 || matriz_madre[i][j] == 12 || matriz_madre[i][j] == 13) {
+                if (matriz_madre[i][j] == 1) {
+                    printf("A");
+                } else if (matriz_madre[i][j] == 11) {
+                    printf("J");
+                } else if (matriz_madre[i][j] == 12) {
+                    printf("Q");
+                } else if (matriz_madre[i][j] == 13) {
+                    printf("K");
+                }
+            } else {
+                printf("%d", matriz_madre[i][j]);
+            }
+            printf("%c ", 3 + i);
+        }
+        printf("\n");
+    }
+
+    // Ordenar los arreglos individuales
+    ordenar(NUM_CARTAS, matriz1);
+    ordenar(NUM_CARTAS, matriz2);
+    ordenar(NUM_CARTAS, matriz3);
+    ordenar(NUM_CARTAS, matriz4);
+
+    // Rellena la matriz madre con los números ordenados
+    for (int i = 0; i < NUM_CARTAS; i++) {
+        matriz_madre[0][i] = matriz1[i];
+        matriz_madre[1][i] = matriz2[i];
+        matriz_madre[2][i] = matriz3[i];
+        matriz_madre[3][i] = matriz4[i];
+    }
+
+    // Imprime los valores de la matriz ordenados
+    printf("\nLas cartas en orden se ubican asi:\n");
+    for (int i = 0; i < NUM_PALOS; i++) {
+        for (int j = 0; j < NUM_CARTAS; j++) {
+            if (matriz_madre[i][j] == 1 || matriz_madre[i][j] == 11 || matriz_madre[i][j] == 12 || matriz_madre[i][j] == 13) {
+                if (matriz_madre[i][j] == 1) {
+                    printf("A");
+                } else if (matriz_madre[i][j] == 11) {
+                    printf("J");
+                } else if (matriz_madre[i][j] == 12) {
+                    printf("Q");
+                } else if (matriz_madre[i][j] == 13) {
+                    printf("K");
+                }
+            } else {
+                printf("%d", matriz_madre[i][j]);
+            }
+            printf("%c ", 3 + i);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
